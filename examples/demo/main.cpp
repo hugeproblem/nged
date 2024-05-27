6531e25679a996fba10997e344300f7763b24731
hugeproblem <yuguo@yuguo.me>
2024-05-27 10:42:13 +0800

    cleanup examples

--- examples/demo/main.cpp
+++ examples/demo/main.cpp
@@ -1,6 +1,3 @@
-// dear imgui - standalone example application for DirectX 11
-// If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
-
 #include <nged/nged.h>
 #include <nged/nged_imgui.h>
 #include <nged/style.h>
@@ -35,23 +32,25 @@ public:
     , numInput(numInput)
     , numOutput(numOutput)
   {
-    uint8_t pixels[64*64*4] = {0};
+    constexpr int res = 64;
+    uint8_t pixels[res * res * 4] = {0};
     for (int y = 0; y < 64; y++)
       for (int x = 0; x < 64; x++)
       {
-        pixels[(y*64+x)*4+0] = x*4;
-        pixels[(y*64+x)*4+1] = y*4;
-        pixels[(y*64+x)*4+2] = rand()&0xff;
-        float d = gmath::distance(nged::Vec2(x,y), nged::Vec2(32,32));
-        pixels[(y*64+x)*4+3] = uint8_t(gmath::clamp((31.f-d)/4.f, 0.f, 1.f)*255.f);
+        const int index = (y * res + x) * 4;
+        pixels[index + 0] = x * 4; // Red
+        pixels[index + 1] = y * 4; // Green
+        pixels[index + 2] = rand() & 0xff; // Blue
+        float d = gmath::distance(nged::Vec2(x,y), nged::Vec2(res / 2, res / 2)); // Distance to center
+        pixels[index + 3] = uint8_t(gmath::clamp((res / 2 - 1 - d) / 4.f, 0.f, 1.f) * 255.f); // Alpha
       }
-    icon = nged::Canvas::createImage(pixels, 64, 64);
+    icon = nged::Canvas::createImage(pixels, res, res);
   }
   nged::sint numMaxInputs() const override { return numInput; }
   nged::sint numOutputs() const override { return numOutput; }
   bool acceptInput(nged::sint port, nged::Node const* srcNode, nged::sint srcPort) const override
   {
-    // to test the sanity check
+    // to test the sanity check, "picky" node only accept link from another "picky" node
     if (srcNode->type() == "picky" && type() == "picky")
       return false;
     return true;
@@ -144,34 +143,7 @@ class DemoApp: public nged::App
 
     ImGui::GetIO().IniFilename = nullptr; // disable window size/pos/layout store
 
-    ImGui::StyleColorsDark();
-    ImVec4* colors = ImGui::GetStyle().Colors;
-    colors[ImGuiCol_FrameBg]            = ImVec4(0.28f, 0.28f, 0.28f, 0.54f);
-    colors[ImGuiCol_FrameBgHovered]     = ImVec4(0.68f, 0.67f, 0.64f, 0.40f);
-    colors[ImGuiCol_FrameBgActive]      = ImVec4(0.45f, 0.45f, 0.45f, 0.67f);
-    colors[ImGuiCol_TitleBgActive]      = ImVec4(0.19f, 0.19f, 0.19f, 1.00f);
-    colors[ImGuiCol_CheckMark]          = ImVec4(0.67f, 0.67f, 0.67f, 1.00f);
-    colors[ImGuiCol_SliderGrab]         = ImVec4(0.89f, 0.89f, 0.89f, 1.00f);
-    colors[ImGuiCol_SliderGrabActive]   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
-    colors[ImGuiCol_Button]             = ImVec4(0.47f, 0.46f, 0.45f, 0.40f);
-    colors[ImGuiCol_ButtonHovered]      = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-    colors[ImGuiCol_ButtonActive]       = ImVec4(0.69f, 0.69f, 0.69f, 1.00f);
-    colors[ImGuiCol_Header]             = ImVec4(0.33f, 0.31f, 0.28f, 0.31f);
-    colors[ImGuiCol_HeaderHovered]      = ImVec4(0.26f, 0.26f, 0.26f, 0.80f);
-    colors[ImGuiCol_HeaderActive]       = ImVec4(0.76f, 0.76f, 0.76f, 1.00f);
-    colors[ImGuiCol_SeparatorHovered]   = ImVec4(0.57f, 0.59f, 0.61f, 0.78f);
-    colors[ImGuiCol_SeparatorActive]    = ImVec4(0.58f, 0.58f, 0.58f, 1.00f);
-    colors[ImGuiCol_ResizeGrip]         = ImVec4(0.48f, 0.48f, 0.48f, 0.25f);
-    colors[ImGuiCol_ResizeGripHovered]  = ImVec4(0.60f, 0.60f, 0.60f, 0.67f);
-    colors[ImGuiCol_ResizeGripActive]   = ImVec4(0.82f, 0.82f, 0.82f, 0.95f);
-    colors[ImGuiCol_Tab]                = ImVec4(0.23f, 0.23f, 0.23f, 0.86f);
-    colors[ImGuiCol_TabHovered]         = ImVec4(0.28f, 0.28f, 0.28f, 0.80f);
-    colors[ImGuiCol_TabActive]          = ImVec4(0.32f, 0.32f, 0.32f, 1.00f);
-    colors[ImGuiCol_TabUnfocused]       = ImVec4(0.05f, 0.05f, 0.05f, 0.97f);
-    colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.22f, 0.22f, 0.22f, 1.00f);
-    colors[ImGuiCol_DockingPreview]     = ImVec4(0.61f, 0.61f, 0.61f, 0.70f);
-    colors[ImGuiCol_TextSelectedBg]     = ImVec4(1.00f, 1.00f, 1.00f, 0.35f);
-    colors[ImGuiCol_NavHighlight]       = ImVec4(0.78f, 0.78f, 0.78f, 1.00f);
+    App::init();
 
     editor = nged::newImGuiNodeGraphEditor();
     editor->setResponser(std::make_shared<nged::DefaultImGuiResponser>());
