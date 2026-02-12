60ca48f3b4909f2b84e06152e7185cf13321b9b6
hugeproblem <yuguo@yuguo.me>
2026-02-12 18:03:50 +0800

    updated build config to include header files in IDE project

--- examples/ngs7/mus-config.h
+++ examples/ngs7/mus-config.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#ifdef __GNUC__
-#define HAVE_OVERFLOW_CHECKS 0
-#else
-#define HAVE_OVERFLOW_CHECKS 0
-#endif
+//#ifdef __GNUC__
+//#define HAVE_OVERFLOW_CHECKS 0
+//#else
+//#define HAVE_OVERFLOW_CHECKS 0
+//#endif
 #define WITH_SYSTEM_EXTRAS 1
 #define WITH_GMP 0
 
